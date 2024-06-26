93fc578025d59aaf6ac7069219a3ac57ce686f8b
KrnlKuro <167052643+s1nse@users.noreply.github.com>
2024-06-26 16:57:36 +0530

    Add files via upload

--- VastV1.lua
+++ VastV1.lua
@@ -1,147 +1,52 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Vast.sh",
-    SubTitle = "Private",
-    TabWidth = 110,
-    Size = UDim2.fromOffset(480, 360),
-    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Title = "S1nse.private",
+    SubTitle = "d1vnity",
+    TabWidth = 90,
+    Size = UDim2.fromOffset(380, 260),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
     Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
 --Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
 local Tabs = {
-    Main = Window:AddTab({
-        Title = "Main", Icon = "home"
-    }),
-    
-    Util = Window:AddTab({
-        Title = "Utility", Icon = "hammer"
-    }),
-    
-    Join = Window:AddTab({
-        Title = "Join", Icon = "user-round-search"
-    }),
-    
-    Dev = Window:AddTab({
-        Title = "Developer", Icon = "eye-off"
-    }),
-    
-    Settings = Window:AddTab({
-        Title = "Settings", Icon = "settings"
-    })
+    Main = Window:AddTab({ Title = "Main", Icon = "file-code-2" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
 local Options = Fluent.Options
 
 do
-Fluent:Notify({
-    Title = "Notification",
-    Content = "This is a notification",
-    SubContent = "SubContent", -- Optional
-    Duration = 5 -- Set to nil to make the notification not disappear
-})
-
-
-
-Tabs.Main:AddParagraph({
-    Title = "Paragraph",
-    Content = "This is a paragraph.\nSecond line!"
-})
-
-
-
-Tabs.Main:AddButton({
-    Title = "Button",
-    Description = "Very important button",
-    Callback = function()
-    Window:Dialog({
-        Title = "Title",
-        Content = "This is a dialog",
-        Buttons = {
-            {
-                Title = "Confirm",
-                Callback = function()
-                print("Confirmed the dialog.")
-                end
-            },
-            {
-                Title = "Cancel",
-                Callback = function()
-                print("Cancelled the dialog.")
-                end
-            }
-        }
+    Fluent:Notify({
+        Title = "S1nse",
+        Content = "Injecting Module"..math.huge.."",
+        SubContent = "SubContent", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
     })
-    end
-})
 
-Tabs.Dev:AddButton({
-    Title = "Iframe [Humanoid Position]",
-    Description = "Gets You The Iframes Of Your Currently Position.",
-    Callback = function()
-    Window:Dialog({
-        Title = "Iframe [Current]",
-        Content = "Gives You Current Standing Location Position",
-        Buttons = {
-            {
-                Title = "Confirm",
-                Callback = function()
-                local player = game.Players.LocalPlayer
-                local character = player.Character or player.CharacterAdded:wait()
-                local humanoidRootPart = character:WaitForChild('HumanoidRootPart')
-
-                local position = humanoidRootPart.Position
-                print(position) -- This would output the position in the output window.
-                setclipboard(tostring(Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position))
-                end
-            },
-            {
-                Title = "Cancel",
-                Callback = function()
-                print("Cancelled the dialog.")
-                end
-            }
-        }
+    Tabs.Main:AddParagraph({
+        Title = "Level:",
+        Content = tonumber(player.PlayerFolder.Stats.Level.Value)
     })
-    end
-})
+    
+    Tabs.Main:AddParagraph({
+        Title = "Level:",
+        Content = tonumber(player.PlayerFolder.Stats.Level.Value)
+    })
 
-local Input = Tabs.Dev:AddInput("Input", {
-    Title = "Teleport[Instant]",
-    Default = "",
-    Placeholder = "UserName",
-    Numeric = false, -- Only allows numbers
-    Finished = true, -- Only calls callback when you press enter
-    Callback = function(modules)
-        print(modules)
-    local player = game.Players.LocalPlayer
-    local usernameToTeleport = modules -- Replace with the desired username
-
-    local function teleportToPlayer()
-    local targetPlayer = game.Players:FindFirstChild(usernameToTeleport)
-    if targetPlayer then
-    player.Character:SetPrimaryPartCFrame(targetPlayer.Character.PrimaryPart.CFrame)
-    else
-        print("Player not found.")
-    end
-    end
-
-    teleportToPlayer()
-    end
-})
-
-Input:OnChanged(function()
-    print("Input updated:", Input.Value)
-    end)
-end
-
-
+    Tabs.Main:AddParagraph({
+        Title = "Level:",
+        Content = tonumber(player.PlayerFolder.Stats.Level.Value)
+    })
+    
+    Tabs.Main:AddParagraph({
+        Title = "Team",
+        Content = (player.PlayerFolder.Customization.Team.Value)
+    })
 -- Addons:
 -- SaveManager (Allows you to have a configuration system)
 -- InterfaceManager (Allows you to have a interface managment system)
@@ -170,9 +75,9 @@ SaveManager:BuildConfigSection(Tabs.Settings)
 Window:SelectTab(1)
 
 Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
+    Title = "S1nse.sh",
+    Content = "Module Injected",
+    Duration = 2
 })
 
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
